0853c88afb332a88e7bf811720b6d111499b7e24
Denys-Omelchuk <denom777@outlook.com>
2022-09-06 11:44:44 +0300

    Add: PostComment model
    Model is named as "PostComment" becouse later there would be one more
    comment section named "RoomComment". Uniqueization of those models makes
    it easier and more comfortable to work with them.

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from base.models import Post, User
+from base.models import Post, PostComment, User
 from models import User 
 
 admin.site.register(User)
 admin.site.register(Post)
+admin.site.register(PostComment)
--- base/models.py
+++ base/models.py
@@ -23,3 +23,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.body
+
+
+class PostComment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    text = models.CharField(max_length=50, null=False)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.text
