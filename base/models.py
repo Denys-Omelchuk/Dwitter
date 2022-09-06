b98f5536114031e9f3f4005d435c231c9f40bc37
Denys-Omelchuk <denom777@outlook.com>
2022-09-06 11:23:51 +0300

    Add User Model
    USERNAME_FIELD was updated, so users login via their emails

--- base/models.py
+++ base/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=25)
+    avatar = models.ImageField(null=True, default='avatar.svg')
+    bio = models.CharField(null=True, max_length=250)
+    email = models.EmailField(null=True, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
