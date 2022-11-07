291db8bb6033252977ed6a2c483e768869659dfb
Denys-Omelchuk <denom777@outlook.com>
2022-11-07 13:19:51 +0200

    Modified: admin panel's fieldsets

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
 from base.models import Post, PostComment, User, Like
- 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin (BaseUserAdmin):
+    list_display = ("username", "email", "name",
+                    "is_staff", "has_posts")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {
+         "fields": ("name", "email", "bio", "has_posts")}),
+        (_("follow"), {"fields": ("follow",)}),
+        (_("followers"), {"fields": ("followers",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
 admin.site.register(Post)
 admin.site.register(PostComment)
 admin.site.register(Like)
